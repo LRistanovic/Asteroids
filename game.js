8a36b30ad5411ac2ff349d6bbe41f12407d419b2
LRistanovic <57436385+LRistanovic@users.noreply.github.com>
2019-11-24 11:10:38 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -700,7 +700,7 @@ function drawScore(){
     ctx.fillText('Score: '+score, 8,20);
     ctx.fillText('Points: '+points, 8,35);
     ctx.fillText('Bullet Speed: '+ship.bulletSpeed, 8,canvas.height-15);
-    ctx.fillText('Bullet Damage: '+ship.Damage, 8,canvas.height-30);
+    ctx.fillText('Bullet Damage: '+ship.bulletDamage, 8,canvas.height-30);
     ctx.fillText('Firerate: '+ship.Reload, 8,canvas.height-45);
     ctx.fillText('Speed: '+ship.Speed, 8,canvas.height-60);
 }
